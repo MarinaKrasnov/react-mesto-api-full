f6206c39ee0d08ba8ffc0e4c8c98571f9add2127
Marina Krasnov <marina.krasnova@gmail.com>
2022-07-30 14:59:53 +0300

    fix auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,10 +3,9 @@ const UnauthorizedError = require('../errors/unauth');
 
 const auth = (req, res, next) => {
   const { cookies } = req;
-  if (!cookies) {
-    next(new UnauthorizedError('jwt is not valid'));
-  } else {
-    const token = cookies.jwt;
+  const { authorization } = req.headers;
+  if (!!cookies || !!authorization) {
+    const token = cookies ? cookies.jwt : authorization.replace('Bearer ', '');
     let payload;
     try {
       payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret');
@@ -16,6 +15,8 @@ const auth = (req, res, next) => {
     if (payload) {
       req.user = payload;
     }
+  } else {
+    next(new UnauthorizedError('jwt is not valid'));
   }
   next()
 }
